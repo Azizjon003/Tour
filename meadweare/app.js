1ffe8005a053f0a43d86e16115bd52b017554d10
azizjon <aliqulovazizjon68@gmail.com>
2022-07-23 13:51:43 +0500

    saqla

--- meadweare/app.js
+++ meadweare/app.js
@@ -5,13 +5,37 @@ const userRouter = require("../routes/userRoute");
 const errorHandler = require("../controller/errorHandler");
 
 const app = express();
+const rateLimit = require("express-rate-limit");
+const halmet = require("helmet");
+const dataSanitize = require("expres-mongo-sanitize");
+const xssClean = reqiure("xss-clean");
 const AppError = require("../utility/apperror");
 const morgan = require("morgan");
-app.use(express.json());
+const limitter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "This IP to many requests",
+});
+
+app.use(halmet());
+app.use("/api", limitter);
+
 app.use(morgan("dev"));
+
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+);
+
+app.use(dataSanitize());
+
+app.use(xssClean());
+app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", Router);
 app.use("/api/v1/users", userRouter);
+
 app.all("*", (req, res, next) => {
   next(new AppError("Not found", 404));
 });
